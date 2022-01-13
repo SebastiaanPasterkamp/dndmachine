41ed900ff910e7561092bf1696d2e552e5f29834
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2022-01-13 22:09:19 +0100

    WIP: First step towards user profile

---
+++ ui/src/partials/ObjectView/ObjectView.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { useParams } from "react-router-dom";
+
+async function getObject(type, id) {
+  return fetch(`/api/${type}/${id}`, {
+    method: 'GET',
+    credentials: 'same-origin',
+  })
+    .then(response => {
+      if (!response.ok) {
+        return null;
+      }
+      return response.json()
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+export default function ObjectView({
+  component: Component,
+  type,
+}) {
+  const { id } = useParams();
+
+  const [error, setError] = React.useState(false);
+  const [object, setObject] = React.useState(null);
+
+  const mounted = React.useRef(true);
+
+  React.useEffect(() => {
+    mounted.current = true;
+
+    if (object || error) {
+      return;
+    }
+
+    getObject(type, id)
+      .then(data => {
+        if (data == null || data.error) {
+          setError(true)
+        } else {
+          setObject(data.result)
+        }
+      })
+
+    return () => mounted.current = false;
+  }, [type, id, error, object])
+
+  return (
+    <Component {...object} />
+  );
+}
---
+++ ui/src/partials/UserCard/UserCard.js
@@ -0,0 +1,122 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Avatar from '@mui/material/Avatar';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import Chip from '@mui/material/Chip';
+import EmailIcon from '@mui/icons-material/Email';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import GroupsIcon from '@mui/icons-material/Groups';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Skeleton from '@mui/material/Skeleton';
+import { faWizardsOfTheCoast } from '@fortawesome/free-brands-svg-icons/faWizardsOfTheCoast';
+
+import { stringToInitials, stringToColor } from '../../utils';
+
+export default function UserCard(props) {
+  const { id, username, name, email, role, dci } = props;
+  const loading = id === undefined;
+  const displayName = name ? name : username;
+
+  return (
+    <Card sx={{ maxWidth: 345, m: 2 }}>
+      <CardHeader
+        avatar={
+          loading ? (
+            <Skeleton animation="wave" variant="circular" width={40} height={40} />
+          ) : (
+            <Avatar
+              sx={{
+                bgcolor: stringToColor(displayName),
+              }}
+              children={stringToInitials(displayName)}
+              aria-label="name"
+            />
+          )
+        }
+        action={
+          loading ? null : (
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          )
+        }
+        title={
+          loading ? (
+            <Skeleton
+              animation="wave"
+              height={10}
+              width="80%"
+              style={{ marginBottom: 6 }}
+            />
+          ) : (
+            displayName
+          )
+        }
+        subheader={
+          loading ? (
+            <Skeleton animation="wave" height={10} width="40%" />
+          ) : (
+            username
+          )
+        }
+      />
+
+      <CardContent>
+        {loading ? (
+          <React.Fragment>
+            <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
+            <Skeleton animation="wave" height={10} width="80%" />
+          </React.Fragment>
+        ) : (
+          <List
+            sx={{ width: '100%', bgcolor: 'background.paper' }}
+            aria-label="contacts"
+          >
+            <ListItem disablePadding>
+              <ListItemIcon>
+                <EmailIcon />
+              </ListItemIcon>
+              <ListItemText primary={email} />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemIcon>
+                <GroupsIcon />
+              </ListItemIcon>
+              <ListItemText primary={role && role.map((r) => (
+                <Chip key={r} label={r} size="small" />
+              ))} />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemIcon>
+                <FontAwesomeIcon icon={faWizardsOfTheCoast} />
+              </ListItemIcon>
+              <ListItemText primary={dci} secondary="DCI" />
+            </ListItem>
+          </List>
+        )}
+      </CardContent>
+    </Card>
+  );
+}
+
+UserCard.propTypes = {
+  id: PropTypes.number,
+  username: PropTypes.string,
+  name: PropTypes.string,
+  email: PropTypes.string,
+  role: PropTypes.arrayOf(
+    PropTypes.string
+  ),
+  dci: PropTypes.string,
+};
+
+UserCard.defaultProps = {
+  role: [],
+};
