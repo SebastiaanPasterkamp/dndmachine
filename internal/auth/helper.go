8ac8bd1813c1686efa975acc548aa75aa53db14c
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2022-01-24 11:24:04 +0100

    Auth: sanitize URL for rego query input

--- internal/auth/helper.go
+++ internal/auth/helper.go
@@ -2,14 +2,19 @@ package auth
 
 import (
 	"net/http"
+	"regexp"
 	"strings"
 
 	"github.com/SebastiaanPasterkamp/dndmachine/internal/model"
 )
 
+var cleanURL = regexp.MustCompile(`[^a-zA-Z0-9/_-]+`)
+
 func getInput(r *http.Request) map[string]interface{} {
 	user, _ := r.Context().Value(CurrentUser).(*model.User)
-	path := strings.Split(strings.Trim(r.URL.Path, "/"), "/")
+	url := strings.Trim(r.URL.Path, "/")
+	url = cleanURL.ReplaceAllString(url, "_")
+	path := strings.Split(url, "/")
 
 	return map[string]interface{}{
 		"path":   path,
