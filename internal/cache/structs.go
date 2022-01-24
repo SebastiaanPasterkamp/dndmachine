91839d7ece2c706347ddcc861ad25a05a57c66dd
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2022-01-24 10:03:41 +0100

    Fix CLI arg annotation for redis cache

--- internal/cache/structs.go
+++ internal/cache/structs.go
@@ -10,6 +10,6 @@ type InMemorySettings struct {
 
 type RedisSettings struct {
 	Address  string `json:"address" arg:"--redis-address,env:DNDMACHINE_REDIS_ADDRESS" placeholder:"host:port" help:"The address of the redis cache service."`
-	Password string `json:"password" arg:"--redis-password,env:DNDMACHINE_REDIS_PASSWORD" help:"The redis passsword to use.`
+	Password string `json:"password" arg:"--redis-password,env:DNDMACHINE_REDIS_PASSWORD" help:"The redis passsword to use."`
 	Database int    `json:"database" arg:"--redis-database,env:DNDMACHINE_REDIS_DATABASE" placeholder:"#" help:"The redis database to use."`
 }
