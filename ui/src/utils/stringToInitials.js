dc1eab8eb33556b7f7279f041e3e2929defa8b62
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2022-01-25 11:14:09 +0100

    Fix Avatar initials; only take first letter of tokens

--- ui/src/utils/stringToInitials.js
+++ ui/src/utils/stringToInitials.js
@@ -4,11 +4,11 @@ export default function stringToInitials(string) {
         return string;
     }
 
-    const parts = string.split(' ');
+    const parts = string.trim().split(/\s+/);
     const items = parts.length;
     if (items > 1) {
         return `${parts[0][0]}${parts[items - 1][0]}`;
     }
 
-    return `${string[0]}${string[strLen - 1]}`;
+    return string[0];
 }
