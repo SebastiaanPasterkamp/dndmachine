7a18ad5d17ece0ec127890cf3333d4f324f9116a
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2022-06-18 18:13:10 +0200

    UI: Move tableSort func to utils

--- ui/src/utils/index.js
+++ ui/src/utils/index.js
@@ -2,10 +2,12 @@ import filterMethod from './filterMethod';
 import stringToInitials from './stringToInitials';
 import stringToColor from './stringToColor';
 import sortMethod from './sortMethod';
+import tableOrder from './tableOrder';
 
 export {
     filterMethod,
     sortMethod,
     stringToInitials,
     stringToColor,
+    tableOrder,
 }
