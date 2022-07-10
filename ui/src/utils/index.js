938c1bf43f205982689f5b34d3ca2e0f66e39aef
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2022-07-10 23:41:03 +0200

    Add uuidv4 utils function

--- ui/src/utils/index.js
+++ ui/src/utils/index.js
@@ -2,10 +2,12 @@ import filterMethod from './filterMethod';
 import stringToInitials from './stringToInitials';
 import stringToColor from './stringToColor';
 import sortMethod from './sortMethod';
+import uuidv4 from './uuidv4';
 
 export {
     filterMethod,
     sortMethod,
     stringToInitials,
     stringToColor,
+    uuidv4,
 }
