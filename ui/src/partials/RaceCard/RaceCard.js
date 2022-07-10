d8bf923d9329c019f7d4c2d8f1469475a3d40a9e
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2022-07-10 22:43:05 +0200

    Switch to BadgeAvatar in RaceCard

--- ui/src/partials/RaceCard/RaceCard.js
+++ ui/src/partials/RaceCard/RaceCard.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import Avatar from '@mui/material/Avatar';
+import BadgedAvatar from '../BadgedAvatar';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardActionArea from '@mui/material/CardActionArea';
@@ -19,14 +19,12 @@ import { useObjectsContext } from '../../context/ObjectsContext';
 import { PolicyLink } from '../ProtectedLink';
 import Skeleton from '@mui/material/Skeleton';
 
-import { stringToInitials, stringToColor } from '../../utils';
-
 export default function RaceCard(props) {
-  const { id, sub, name, description } = props;
+  const { id, sub, name, description, avatar } = props;
 
   const mounted = React.useRef(true);
   const { race: races } = useObjectsContext();
-  const [parentName, setParentName] = React.useState(null);
+  const [parent, setParent] = React.useState(null);
   const loading = id === undefined;
 
   const [expanded, setExpanded] = React.useState(false);
@@ -46,23 +44,24 @@ export default function RaceCard(props) {
       .catch((error) => console.error('Error:', error));
   }
 
-
   React.useEffect(() => {
     if (!mounted.current) {
       return () => mounted.current = false;
     }
 
     if (!sub) {
-      setParentName(null);
+      setParent(null);
       return;
     }
 
     if (!races.length) {
-      setParentName(null);
+      setParent(null);
     }
 
-    const { name = null } = races[sub] || {};
-    if (mounted.current) setParentName(name);
+    const { name, avatar } = races[sub] || {};
+    if (mounted.current) {
+      setParent({ name, avatar });
+    }
 
     return () => mounted.current = false;
   }, [sub, races])
@@ -75,12 +74,10 @@ export default function RaceCard(props) {
             loading ? (
               <Skeleton animation="wave" variant="circular" width={40} height={40} />
             ) : (
-              <Avatar
-                sx={{
-                  bgcolor: stringToColor(name),
-                }}
-                children={stringToInitials(name)}
-                aria-label="name"
+              <BadgedAvatar
+                name={name}
+                avatar={avatar}
+                badge={parent}
               />
             )
           }
@@ -99,7 +96,7 @@ export default function RaceCard(props) {
           subheader={
             loading ? (
               <Skeleton animation="wave" height={10} width="40%" />
-            ) : parentName
+            ) : (parent ? parent.name : null)
           }
         />
 
@@ -170,10 +167,8 @@ export default function RaceCard(props) {
 
 RaceCard.propTypes = {
   id: PropTypes.number,
+  sub: PropTypes.number,
   name: PropTypes.string,
   description: PropTypes.string,
-};
-
-RaceCard.defaultProps = {
-  role: [],
+  avatar: PropTypes.string,
 };
