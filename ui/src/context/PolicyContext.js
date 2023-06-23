e215824b0a2ef92702be7cb41071b1e16b268f13
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2023-06-23 22:56:04 +0200

    ui: split policy context from opa engine

--- ui/src/context/PolicyContext.js
+++ ui/src/context/PolicyContext.js
@@ -1,49 +1,29 @@
 import * as React from 'react';
-import opa from "@open-policy-agent/opa-wasm";
+import { usePolicyEngineContext } from './PolicyEngineContext';
 
 const Policy = React.createContext({});
 
-async function getPolicy() {
-  return fetch('/ui/policy.wasm')
-    .then(response => response.arrayBuffer())
-    .then(wasm => opa.loadPolicy(wasm))
-}
-
 export default function PolicyContext({ data, useContext = () => { }, query, children }) {
-  const [policy, setPolicy] = React.useState(null);
+  const { policy } = usePolicyEngineContext();
   const funcData = useContext();
 
   const isAllowed = (input, override) => {
     if (!policy) {
       return false;
     }
+    policy.setData({ ...data, ...funcData });
     const rules = policy.evaluate(input, override || query);
     for (const i in rules) {
-      if (rules[i].result) {
+      // result can be non-boolean, which means it is (partially) allowed
+      if (rules[i].result !== false) {
         return true
       }
     }
     return false
   }
 
-  const mounted = React.useRef(true);
-
-  React.useEffect(() => {
-    mounted.current = true;
-
-    getPolicy()
-      .then(policy => {
-        if (!mounted.current) return;
-
-        policy.setData({ ...data, ...funcData });
-        setPolicy(policy)
-      })
-
-    return () => mounted.current = false;
-  }, [data, funcData])
-
   return (
-    <Policy.Provider value={{ policy, isAllowed }}>
+    <Policy.Provider value={{ isAllowed }}>
       {children}
     </Policy.Provider>
   );
