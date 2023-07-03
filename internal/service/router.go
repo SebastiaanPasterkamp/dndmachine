ddd745bc88dcec1f037cee7daa19ed9ce12fb1a3
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2023-07-03 18:42:30 +0200

    chore(api): add compression for wasm

--- internal/service/router.go
+++ internal/service/router.go
@@ -5,14 +5,29 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
 	"github.com/SebastiaanPasterkamp/dndmachine/internal/api"
 	"github.com/SebastiaanPasterkamp/dndmachine/internal/auth"
 	"github.com/SebastiaanPasterkamp/dndmachine/internal/cache"
 	"github.com/SebastiaanPasterkamp/dndmachine/internal/database"
 	"github.com/SebastiaanPasterkamp/dndmachine/internal/policy"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
+
+var compressibleContentTypes = []string{
+	"text/html",
+	"text/css",
+	"text/plain",
+	"text/javascript",
+	"application/javascript",
+	"application/x-javascript",
+	"application/wasm",
+	"application/json",
+	"application/atom+xml",
+	"application/rss+xml",
+	"image/svg+xml",
+}
 
 func (s *Instance) Router(ctx context.Context, db database.Instance) (*chi.Mux, error) {
 	repo, err := cache.Factory(ctx, s.Configuration)
@@ -32,7 +47,7 @@ func (s *Instance) Router(ctx context.Context, db database.Instance) (*chi.Mux, 
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.Compress(5))
+	r.Use(middleware.Compress(9, compressibleContentTypes...))
 
 	// Set a timeout value on the request context (ctx), that will signal
 	// through ctx.Done() that the request has timed out and further
